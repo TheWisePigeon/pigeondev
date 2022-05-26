34b15533f335f933d279bcfcab1f380379a364dc
TheWisePigeon <zozozozeph@gmail.com>
2022-05-26 22:45:09 +0000

    added files

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Pigeondev portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+    </div>
+  )
+}
